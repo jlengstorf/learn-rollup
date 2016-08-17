7f96c0a0547cf05ea0ae13a69298aec674881622
jlengstorf <jason.lengstorf@copterlabs.com>
2016-08-17 13:32:28 +0200

    feat(step-4): added support for third-party Node packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 // Rollup plugins
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'src/scripts/main.js',
@@ -8,6 +10,12 @@ export default {
   format: 'iife',
   sourceMap: 'inline',
   plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
     eslint({
       exclude: [
         'src/styles/**',
