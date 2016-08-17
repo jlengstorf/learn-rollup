d465562ec85b5e0abe8978074580c019b775e5bf
jlengstorf <jason.lengstorf@copterlabs.com>
2016-08-17 12:19:55 +0200

    feat(step-3): added ESLint

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 // Rollup plugins
 import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
 
 export default {
   entry: 'src/scripts/main.js',
@@ -7,6 +8,11 @@ export default {
   format: 'iife',
   sourceMap: 'inline',
   plugins: [
+    eslint({
+      exclude: [
+        'src/styles/**',
+      ]
+    }),
     babel(),
   ],
 };
