4824954cd18d89fa003e759a989883f2b1a28bad
jlengstorf <jason.lengstorf@copterlabs.com>
2016-08-17 11:54:59 +0200

    feat(step-2): Added Babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,12 @@
+// Rollup plugins
+import babel from 'rollup-plugin-babel';
+
 export default {
   entry: 'src/scripts/main.js',
   dest: 'build/js/main.min.js',
   format: 'iife',
   sourceMap: 'inline',
+  plugins: [
+    babel(),
+  ],
 };
