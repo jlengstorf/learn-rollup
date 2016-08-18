af23d46a5685f8d69715e40a0b9eb1d6f13c29a5
jlengstorf <jason.lengstorf@copterlabs.com>
2016-08-18 15:25:54 +0200

    feat(livereload): the bundle is rebuilt and browser reloaded on save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,6 +15,12 @@ if (ENV !== 'production') {
   // Enable the logger.
   debug.enable('*');
   log('Logging is enabled!');
+
+  // Enable LiveReload
+  document.write(
+    '<script src="http://' + (location.host || 'localhost').split(':')[0] +
+    ':35729/livereload.js?snipver=1"></' + 'script>'
+  );
 } else {
   debug.disable();
 }
