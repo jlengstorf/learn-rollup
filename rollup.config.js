42065f4893a6dae2811c4319691161a9eeeaf838
jlengstorf <jason.lengstorf@copterlabs.com>
2016-08-18 14:18:03 +0200

    feat(postcss): added stylesheet processing

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,13 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
+import postcss from 'rollup-plugin-postcss';
+
+// PostCSS plugins
+import simplevars from 'postcss-simple-vars';
+import nested from 'postcss-nested';
+import cssnext from 'postcss-cssnext';
+import cssnano from 'cssnano';
 
 export default {
   entry: 'src/scripts/main.js',
@@ -12,6 +19,15 @@ export default {
   format: 'iife',
   sourceMap: 'inline',
   plugins: [
+    postcss({
+      plugins: [
+        simplevars(),
+        nested(),
+        cssnext({ warnForDuplicates: false, }),
+        cssnano(),
+      ],
+      extensions: [ '.css' ],
+    }),
     resolve({
       jsnext: true,
       main: true,
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,6 @@
+// Import styles (automatically injected into <head>).
+import '../styles/main.css';
+
 // Import a couple modules for testing.
 import { sayHelloTo } from './modules/mod1';
 import addArray from './modules/mod2';
