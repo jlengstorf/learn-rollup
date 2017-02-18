0c2539cf6cf2c82bc11c061b4af4fbd3e6c374e5
Robin Schneider <ypid@riseup.net>
2017-02-18 17:46:30 +0100

    Fix comment to actually correspond to the code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,7 +9,7 @@ import addArray from './modules/mod2';
 import debug from 'debug';
 const log = debug('app:log');
 
-// The logger should only be disabled if we’re not in production.
+// The logger should only be enabled if we’re not in production.
 if (ENV !== 'production') {
 
   // Enable the logger.
